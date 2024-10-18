c2dd76538da53276d029ee123f267ec47a0d00f0
zubari1995 <166775989+zubari1995@users.noreply.github.com>
2024-10-19 01:23:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["👑2PAC👑", "🎮 Playing VALORANT"];
+const statusMessages = ["👑2PAC👑", "🎮 Playing"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
